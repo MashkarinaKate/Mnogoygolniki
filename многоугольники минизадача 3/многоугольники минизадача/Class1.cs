9734c9975f4045b669828c63a85098b3b1a5dd93
MashkarinaKate <mashkarinakate@gmail.com>
2020-11-24 20:55:12 +0300

    3

---
+++ многоугольники минизадача 3/многоугольники минизадача/Class1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace многоугольники_минизадача
+{
+    abstract class Вершина
+    {
+        protected static int r;
+        protected int x;
+        protected int y;
+        protected static Color col;
+        public Вершина(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        static Вершина()
+        {
+            r = 25;
+            col = Color.Black;
+        }
+        public abstract void Draw(Graphics graf);
+        public abstract int R { get; set; }
+        public abstract int X { get; set; }
+        public abstract int Y { get; set; }
+        public abstract bool Check(int x, int y);
+    }
+    class Круг : Вершина
+    {
+        public Круг(int x, int y): base(x, y) { }
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(col), x - r/2, y - r/2, r, r);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (Math.Pow(this.x - x, 2) + Math.Pow(this.y - y, 2) <= Math.Pow(r, 2))
+                return true;
+            else return false;
+        }
+        public override int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public override int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public override int R
+        {
+            get { return r; }
+            set
+            {
+                if (value > 0)
+                {
+                    r = value;
+                }
+            }
+        }
+    }
+    class Квадрат : Вершина
+    {
+        public Квадрат(int x, int y) : base(x, y) { }
+        public override void Draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(col), x - r/2, y - r/2, r, r);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (Math.Abs(x - x) <= r && Math.Abs(y - y) <= r) return true;
+            else return false;
+        }
+        public override int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public override int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public override int R
+        {
+            get { return r; }
+            set
+            {
+                if (value > 0)
+                {
+                    r = value;
+                }
+            }
+        }
+    }
+    class Треугольник : Вершина
+    {
+        public Треугольник(int x, int y) : base(x, y) { }
+        public override void Draw(Graphics g)
+        {
+            Point[] s = new Point[3];
+            s[0] = new Point(x, y - r / 2);
+            s[1] = new Point(x - r/2, y + r / 2);
+            s[2] = new Point(x + r/2, y + r / 2);
+            g.FillPolygon(new SolidBrush(col), s);
+        }
+        public override bool Check(int x, int y)
+        {
+            int i = (this.x - x) * (this.y + r / 2 - (this.y - r / 2)) - (- r / 2 * (this.y - r / 2 - y));
+            int j = 0 - (this.x + r/2 - (this.x - r/2))*(this.y + r/2 - y);
+            int l = (this.x + r / 2 - x)*(this.y - r/2 - (this.y + r/2)) - (this.x - (this.x + r/2))*(this.y + r/2 - y);
+            if ((i >= 0 && j >= 0 && l >= 0) || (i <= 0 && j <= 0 && l <= 0))
+                return true;
+            else return false;
+        }
+        public override int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public override int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public override int R
+        {
+            get { return r; }
+            set
+            {
+                if (value > 0)
+                {
+                    r = value;
+                }
+            }
+        }
+    }
+}
---
+++ многоугольники минизадача 3/многоугольники минизадача/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace многоугольники_минизадача
+{
+    public partial class Form1 : Form
+    {
+        int delx;
+        int dely;
+        bool dr;
+        bool drag;
+        int num;
+
+        List<Вершина> вершины;
+        public Form1()
+        {
+            InitializeComponent();
+            кругToolStripMenuItem.CheckOnClick = true;
+            квадратToolStripMenuItem.CheckOnClick = true;
+            треугольникToolStripMenuItem.CheckOnClick = true;
+            кругToolStripMenuItem.Checked = true;
+            вершины = new List<Вершина>();
+            dr = false;
+            drag = false;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (dr)
+            {
+                foreach(Вершина t in вершины)
+                {
+                    t.Draw(e.Graphics);
+                }
+            }
+        }
+
+        private void КругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            квадратToolStripMenuItem.Checked = false;
+            треугольникToolStripMenuItem.Checked = false;
+            num = 0;
+        }
+
+        private void КвадратToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            кругToolStripMenuItem.Checked = false;
+            треугольникToolStripMenuItem.Checked = false;
+            num = 1;
+        }
+
+        private void ТреугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            квадратToolStripMenuItem.Checked = false;
+            кругToolStripMenuItem.Checked = false;
+            num = 2;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int size;
+            if (вершины.Count == 0) size = 1;
+            else size = вершины.Count;
+            for (int i = 0; i < size; ++i)
+            {
+                if (dr && вершины[i].Check(e.X, e.Y))
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        delx = e.X - вершины[i].X;
+                        dely = e.Y - вершины[i].Y;
+                        drag = true;
+                    }
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        вершины[i] = null;
+                        вершины.RemoveAt(i);
+                    }
+                }
+
+                else
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        switch (num)
+                        {
+                            case 0: вершины.Add(new Круг(e.X, e.Y)); break;
+                            case 1: вершины.Add(new Квадрат(e.X, e.Y)); break;
+                            case 2: вершины.Add(new Треугольник(e.X, e.Y)); break;
+                            default: вершины.Add(new Круг(e.X, e.Y)); break;
+                        }
+                        dr = true;
+                    }
+                }
+            }
+            this.Refresh();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < вершины.Count; i++)
+            {
+                if (drag && вершины[i].Check(e.X, e.Y))
+                {
+                    вершины[i].X = e.X - delx;
+                    вершины[i].Y = e.Y - dely;
+                    this.Refresh();
+
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                drag = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
